015588b455a29f370051a05e9b783e7bbb19bf5a
Miguel Alvarez <kamapu78@gmail.com>
2023-11-25 21:44:09 +0100

    2 function do release (#8)
    * Draft for db_release.
    
    * New function release_project()

--- R/imports.R
+++ R/imports.R
@@ -3,4 +3,6 @@
 #' @importFrom RPostgres Postgres
 #' @importFrom tcltk tclVar tclvalue tkbind tkbutton tkdestroy tkentry tkfocus
 #'     tkgrid tkgrid.configure tklabel tktoplevel tkwait.window tkwm.title
+#' @importFrom yaml read_yaml write_yaml
+#' @importFrom zip zip
 NULL
--- lab/test-script.R
+++ lab/test-script.R
@@ -4,12 +4,15 @@
 ################################################################################
 
 library(backpackR)
+library(zip)
 
+# Start a project
 init_project("lab/my-project", "test-db")
+
+# do a release
+release_project("lab/my-project", "lab")
 
 ## use_template("lab", "main-script")
 ## use_template("lab/copy-main-script.R", "main-script")
 
 #db_backup(dbname = "test-db", file = "test_bk", path="lab")
-
-
