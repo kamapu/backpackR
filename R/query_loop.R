5371eb1544d52f79446e0b3e941c317592d6cfa4
Miguel Alvarez <kamapu78@gmail.com>
2024-01-07 13:46:09 +0100

    Function 'query_loop()' added to the package. (#17)

--- R/query_loop.R
+++ R/query_loop.R
@@ -1,3 +1,29 @@
+#' @name query_loop
+#'
+#' @title Run a query across backup versions
+#'
+#' @description
+#' Queries across versions may be useful to display the evolution of databases
+#' in the time.
+#'
+#' @param path A character value with the path to the collection of releases
+#'     (backups).
+#' @param dbname A character value with the name of the database.
+#' @param statement A character value with a SQL statement. This will be passed
+#'     to [dbSendQuery()].
+#' @param host A character value with the host of the database.
+#' @param port A character value with the port of the database.
+#' @param user A character value with the name of the user connecting the
+#'     database. If not provided, it will be prompted by [credentials()].
+#' @param password A character value with the password of the user. If not
+#'     provided, it will be prompted by [credentials()].
+#' @param ... Further argunents passed to [build_db()].
+#'
+#' @return
+#' A data frame resulting from applying [rbind()] to every run output from
+#' [dbGetQuery()].
+#'
+#' @export
 query_loop <- function(
     path, dbname, statement,
     ## auxiliar_db = "temp-db",
@@ -20,7 +46,7 @@ query_loop <- function(
       overwrite = TRUE, host = host, port = port,
       user = user, password = password, ...
     )
-    Sys.sleep(0.5)
+    # Sys.sleep(0.5)
     conn <- connect_db(
       dbname = dbname,
       ## dbname = auxiliar_db,
@@ -30,5 +56,6 @@ query_loop <- function(
     out_obj[[i]] <- dbGetQuery(conn, statement)
     message(paste("step", i))
   }
-  return(out_obj)
+  # return(out_obj)
+  return(do.call(rbind, out_obj))
 }
--- lab/ex-query_loop.R
+++ lab/ex-query_loop.R
@@ -3,52 +3,8 @@
 # Author: Miguel Alvarez
 ################################################################################
 
+devtools::install()
 library(backpackR)
-library(divDB)
-library(RPostgres)
-
-source("R/query_loop.R")
 
 query_loop(path = "lab/exp-references", dbname = "mreferences",
     statement = paste("select count(*)", "from bibliorefs.main_table"))
-
-path = "lab/exp-references"
-dbname = "mreferences"
-statement = paste("select count(*)", "from bibliorefs.main_table")
-
-user = ""
-password = ""
-
-i <- tab_rel$nr[1]
-i
-
-
-
-
-
-
-
-
-
-conn <- connect_db("mreferences")
-dbGetQuery(conn, paste("select count(*)", "from bibliorefs.main_table"))
-
-
-
-DBI::dbDisconnect(conn)
-
-
-
-tab_rel <- sort_releases(path = "lab/exp-references", dbname = "mreferences")
-
-
-path <- "lab/exp-references"
-dbname <- "mreferences"
-statement <- paste("select count(*)", "from bibliorefs.main_table")
-host <- "localhost"
-port <- "5432"
-auxiliar_db <- "temp-db2"
-user <- ""
-password <- ""
-
-i <- tab_rel$nr[1]
